d29f3fb77883cadd235d363b724cd4819c4bac2c
Matthew Alltop <Matthew.Alltop@salliemae.com>
2019-11-21 11:06:31 -0500

    Fixed API

--- src/main.py
+++ src/main.py
@@ -1,33 +1,39 @@
 from blockchain import Blockchain
+import json
 from uuid import uuid4
-from flask import Flask
+from flask import Flask, jsonify
 
 
-def main():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    # Generate a globally unique address for this node
-    node_identifier = str(uuid4()).replace('-', '')
 
-    # Instantiate the Blockchain
-    blockchain = Blockchain()
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+# Instantiate the Blockchain
+blockchain = Blockchain()
+@app.route("/")
+def home():
+    return "Hello, Flask!"
 
-    @app.route('/mine', methods=[GET])
-    def mine():
-        return "We'll mine a new Block"
 
-    @app.route('/transactions/new', methods=[POST])
-    def new_transaction():
-        return "We'll add a new transaction"
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
 
-    @app.route('/chain', methods=[GET])
-    def full_chain():
-        response = {
-            'chain': blockchain.chain,
-            'length': len(blockchain.chain)
-        }
-        return jsonify(response), 200
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='127.0.0.1', port=5000, threaded=True)
